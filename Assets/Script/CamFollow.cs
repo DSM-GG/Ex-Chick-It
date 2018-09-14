eb15dec147b36f4603082803e43a642a9ee1929b
Unknown <tlathdnr@gmail.com>
2018-09-14 17:11:40 +0900

    del blocks

---
+++ Assets/Script/CamFollow.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class CamFollow : MonoBehaviour {
+
+    [SerializeField]
+    private Transform player;
+
+    private void FixedUpdate() {
+        Vector3 moveVec = Vector2.Lerp(transform.position, player.position, Time.deltaTime * 3);
+        transform.position = new Vector3(moveVec.x, moveVec.y, -10);
+    }
+}
---
+++ Assets/Script/Player.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField]
+    private JoyStick joyStick;
+
+    [SerializeField]
+    private GameObject winScreen, loseScreen;
+
+    [SerializeField]
+    private float speed;
+
+    private static bool bControl = true;
+
+    private void Update() {
+        if (gameObject.name != "Player") return;
+        if (!bControl) return;
+
+        Vector3 moveVec = Vector3.zero;
+        moveVec.x = joyStick.Horizontal;
+        moveVec.y = joyStick.Vertical;
+
+        transform.position += moveVec * speed * Time.deltaTime;
+        transform.eulerAngles = new Vector3(0, 0, joyStick.Angle);
+    }
+
+    private void OnCollisionStay2D(Collision2D col) {
+        StopCoroutine("KnockBack");
+    }
+
+    private void OnTriggerEnter2D(Collider2D col) {
+        if (col.CompareTag("Escape")) {
+            bControl = false;
+            winScreen.SetActive(true);
+        }
+    }
+
+    public void Hit(Vector3 destPos) {
+        StartCoroutine("KnockBack", destPos);
+    }
+
+    public void Death() {
+        bControl = false;
+        loseScreen.SetActive(gameObject.name == "Player");
+        winScreen.SetActive(gameObject.name != "Player");
+    }
+
+    private IEnumerator KnockBack(Vector3 destPos) {
+        for (float passTime = 0; passTime < 0.5f; passTime += Time.deltaTime) {
+            Vector3 movePos = Vector3.Lerp(transform.position, destPos, Time.deltaTime * 10);
+            transform.position = movePos;
+            yield return null;
+        }
+    }
+}
